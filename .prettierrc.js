188a1d1a724971b068e60f1004a3bd8ea839a742
StenSOn27 <tarashevchik27@gmail.com>
2026-09-22 14:38:33 +0300

    chore: add prettier configs

---
+++ .prettierrc.js
@@ -0,0 +1,21 @@
+export default {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  proseWrap: 'always',
+  overrides: [
+    {
+      files: ['*.json', '.*rc'],
+      options: {
+        parser: 'json5',
+        quoteProps: 'preserve',
+        singleQuote: false,
+        trailingComma: 'none',
+      },
+    },
+  ],
+};
